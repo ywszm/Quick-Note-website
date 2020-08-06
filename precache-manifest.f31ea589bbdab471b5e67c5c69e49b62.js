ea34f0acb98fe77e7aa86ba89f490eec5b462e15
ywszm <aichengwan1@163.com>
2020-08-06 12:18:27 +0800

    deploy

---
+++ precache-manifest.f31ea589bbdab471b5e67c5c69e49b62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2e0870955fcbd9980c",
+    "url": "/Quick-Note-website/css/app.49beb276.css"
+  },
+  {
+    "revision": "96f6a0ef31ef327f7d227566d2da91b1",
+    "url": "/Quick-Note-website/index.html"
+  },
+  {
+    "revision": "7a2e0870955fcbd9980c",
+    "url": "/Quick-Note-website/js/app.f055e70a.js"
+  },
+  {
+    "revision": "a18bf7c61b9cdce0930a",
+    "url": "/Quick-Note-website/js/chunk-vendors.70d1447d.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/Quick-Note-website/manifest.json"
+  },
+  {
+    "revision": "f74831144020acec4ab0bb0e004ac15f",
+    "url": "/Quick-Note-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Quick-Note-website/robots.txt"
+  }
+]);
